7510439719a073cf4381a1e3517f619678c1b547
lmarusich <lmarusich@users.noreply.github.com>
2021-03-25 23:03:53 -0500

    edited paper, made option for legend title, chagned annotation font size

--- JOSS paper/paper.Rmd
+++ JOSS paper/paper.Rmd
@@ -32,15 +32,41 @@ journal: JOSS
 
 # Summary
 
-The most common techniques for calculating the correlation (e.g., Pearson correlation) between two variables are based on the assumption that each data point of paired measures represents an independent observation. For example, a study might calculate the correlation between the volume of a brain area and a person's age. In this example, each individual contributes a data point consisting of their brain volume and their age. However, it is not uncommon for studies to use
-repeated measures designs, such as a study that collecting the area of brain volume and age at two different time points [@raz2005regional]. In this example, each person contributes two (repeated) data points of paired measures. Repeated measures of the same individual are no longer independent observations. Erroneously modeling repeated measures data as independent observations is surprisingly prevalent in published research, even though such results will generally be misleading [@aarts2014solution; @lazic2010problem; @bakdash2020statistical]. A common solution to this problem is to use aggregated data by taking an average of the repeated measures data of each person, making the data independent, and then correlating these averages.  
+The most common techniques for calculating the correlation (e.g., Pearson correlation)
+between two variables are based on the assumption that each data point of paired measures
+represents an independent observation. Take, for example, a study that calculates the 
+correlation between a person's age and the volume of a region of the brain. In this
+example, each individual contributes a data point consisting of a brain volume and an age.
+However, it is not uncommon for studies to use repeated measures designs, such as a study
+that collected the brain region volume and age at two different time points [@raz2005regional].
+Each participant in this study contributed two (repeated) data points of paired measures.
+Repeated measures of the same individual are no longer independent observations and should
+not be analyzed as such. Erroneously modeling repeated measures data as independent
+observations is surprisingly prevalent in published research, even though such results
+will generally be misleading [@aarts2014solution; @lazic2010problem; @bakdash2020statistical].
+A common solution to this problem is to use aggregated data: first taking an average of the 
+repeated measures data of each person so that each person again contributes a single data
+point, and then calculating the correlation from these averages.  
 
-Instead of aggregation, an alternative solution is to calculate the repeated measures correlation [@Bakdash2017; @Bland446; @Bland633], which assesses the common intra-individual (within-participants) association for paired repeated measures data. The repeated measures correlation technique is conceptually similar to a null multilevel model, with a common slope but varying intercept for each individual. Calculating the repeated measures correlation has multiple potential benefits. 
-It is simpler and more straightforward to implement than a multilevel model, with the potential for far greater statistical power than aggregation, and possible insights into patterns among individuals that may otherwise be obscured by aggregation [@Bakdash2017]. 
+Instead of aggregation, an alternative solution is to calculate the repeated measures
+correlation [@Bakdash2017; @Bland446; @Bland633], which assesses the common
+intra-individual (within-participants) association for paired repeated measures data. The
+repeated measures correlation technique is conceptually similar to a null multilevel model,
+with a common slope but varying intercept for each individual. Calculating the repeated
+measures correlation has multiple potential benefits. It is simpler and more
+straightforward to implement than a multilevel model, with the potential for far greater
+statistical power than aggregation. It also has the potential to provide insights into
+patterns among individuals that aggregation may obscure [@Bakdash2017]. 
 
-We previously developed the ``rmcorr`` R package [@R-rmcorr] to make this technique widely available for researchers; it has since also been adapted as a function in the Pingouin statistics package [@Vallat2018] for Python. However, the use of both of these packages requires some facility with  programming languages and thus they not easily accessible to all researchers. 
+We previously developed the ``rmcorr`` R package [@R-rmcorr] to make the repeated measures
+correlation technique widely available for researchers; it has since also been adapted as 
+a function in the Pingouin statistics package [@Vallat2018] for Python. However, the use 
+of both of these packages requires some facility with  programming languages and thus they 
+are not universally accessible. 
 
-We have created the ``rmcorrShiny`` application, which provides an intuitive graphical interface for computing and plotting the repeated measures correlation (see Figure 1 below with brain area volume and age from [@raz2005regional]). 
+Here we introduce the ``rmcorrShiny`` application, which provides an intuitive graphical
+interface for computing and plotting the repeated measures correlation (see Figure 1 below
+demonstrating the application using brain area volume and age data from Raz [-@raz2005regional]). 
 
 The primary features of ``rmcorrShiny`` include:
 
@@ -55,14 +81,21 @@ results.
 pasted and executed in R to produce the same output as in ``rmcorrShiny``.
 * The ability to download plots (in a variety of file formats) or a .zip file of all output.
 
-Note that many features in ''rmcorrShiny'' were borrowed or based on modifications of code in the Raincloud-shiny app [@raincloudshiny] (not sure the best way to phrase this, or cite: Jon- How does this sound now?) 
+Note that many features in ''rmcorrShiny'' were based on modifications of code in the 
+``Raincloud-shiny`` app [@raincloudshiny] \textcolor{red}{(Laura: I like the new language, but what do you think about the citation?
+I found his name through Twitter, but maybe it's kind of creepy to do that? He doesn't have his name on the app or on the github) }
 
-Screenshot (placeholder until we have a near-final version?)
+\textcolor{red}{Screenshot (placeholder until we have a near-final version?)}
 
 
-``rmcorrShiny`` can be used in a web browser [**here**](https://lmarusich.shinyapps.io/shiny_rmcorr/)
-(should we make this into a package too? i saw other people do that. then people can run it locally)
-(also, should we change the name of the repo to match the app? hyphen versus underscore)
+``rmcorrShiny`` can be used in a web browser [**here**](https://lmarusich.shinyapps.io/shiny_rmcorr/) 
+or it can be installed from Github and run in R, using the following commands \textcolor{red}{(BIG TODO):}
+
+```r
+devtools::install_github("lmarusich/rmcorrShiny")
+library(rmcorrShiny)
+rmcorrShiny::rmcorrShiny_run()
+```
 
 
 # Acknowledgements
@@ -73,6 +106,6 @@ the U.S. Army Combat Capabilities Development Command Army Research Laboratory o
 Government. The U.S. Government is authorized to reproduce and distribute reprints for 
 Government purposes notwithstanding any copyright notation. 
 
-We acknowledge contributions from.... 
+\textcolor{red}{We acknowledge contributions from.... }
 
 # References
--- app.R
+++ app.R
@@ -15,15 +15,20 @@ source("source/createPlot.R", local = TRUE)
 source("source/paletteColours.R", local = TRUE)
 
 #Items to add:
-#2) Need warnings for: missing data, non-numeric input in X and Y? 
+#2) Need warnings for: missing data, non-numeric input in X and Y?
 #4) More less done: Palettes and plots
-#c) Jon to-do item: Cut off the first color in sequential by default? The first color is usually too faint to see 
+#c) Jon to-do item: Cut off the first color in sequential by default? The first color is usually too faint to see
 #5) How to handle p-values that round to zero? Round to p < 0.001 instead? Add sci notation elsewhere for exact value in Output at the top?
 #6) Overall fit using data averaged by participant? Output OLS regression and option to add to plots?
+#LM: this is interesting (#6), but i lean towards it being out of scope for this app.
 #7) add option to change legend label
 #8) Partially done (need to pad location): Additional plot options: Print stats on plot and specify the location where it's printed
+#LM: I experimented with letting the user adjust the font size of the annotations, but it turns out that
+#will interact with the padding you've implemented (bigger font sizes start moving closer to the center)
+#so I think it's best to just set a single font size for that, and if they really need to change it, they
+#can grab the R code we provide and do it themselves in an R session.
 
-#Jon: I can record a video tutorial and we can embed in Shiny- maybe in about? An interactive tutorial in Shiny would be really challenging to code  
+#Jon: I can record a video tutorial and we can embed in Shiny- maybe in about? An interactive tutorial in Shiny would be really challenging to code
 
 #Pie in the sky items
 #1) Power calculation: Could be an additional panel?
@@ -36,7 +41,7 @@ dark  <- bs_theme(bg = "black", fg = "white", primary = "lightblue")
 ui <- fluidPage(
   theme = light,
   div(
-    class = "custom-control custom-switch", 
+    class = "custom-control custom-switch",
     tags$input(
       id = "dark_mode", type = "checkbox", class = "custom-control-input",
       onclick = HTML("Shiny.setInputValue('dark_mode', document.getElementById('dark_mode').value);")
@@ -45,11 +50,11 @@ ui <- fluidPage(
   # CSS, fixes palette picker
   tags$style(".bootstrap-select .dropdown-menu li a span.text {width: 100%;}
              #downloadPlot, #downloadZip {margin-top: 25px}"),
-  
-  #Title 
+
+  #Title
   titlePanel("Shiny Repeated Measures Correlation"),
-  
-  # Sidebar  
+
+  # Sidebar
   sidebarLayout(
     sidebarPanel(
       tabsetPanel(
@@ -65,7 +70,7 @@ ui <- fluidPage(
                                    uiOutput('DataFilterColumnsUI')) ),
                    tabPanel("Data options",
                             column(12,
-                                   sliderInput("CIlevel", 
+                                   sliderInput("CIlevel",
                                                label = h5("Confidence Intervals"),
                                                min = 0.5,
                                                max = 0.99,
@@ -87,7 +92,7 @@ ui <- fluidPage(
                  )
         )
         ,
-        tabPanel("Plot Options", 
+        tabPanel("Plot Options",
                  hr()
                  ,
                  tabsetPanel(
@@ -105,6 +110,13 @@ ui <- fluidPage(
                                    checkboxInput("plotLegend",
                                                  "Show Legend",
                                                  FALSE),
+                                   conditionalPanel(
+                                     condition = 'input.plotLegend == true',
+                                     uiOutput('legendTitleUI')
+                                     # textInput("legendTitle",
+                                     #           label = "Legend Title",
+                                     #           value = "Legend Title")
+                                   ),
                                    h5("Annotations"),
                                    checkboxInput("addText",
                                                  "Add rmcorr output text",
@@ -123,7 +135,7 @@ ui <- fluidPage(
                                    checkboxInput("plotMajorGrid",
                                                  "Plot Major Grid",
                                                  FALSE),
-                                   
+
                                    conditionalPanel(
                                      condition = 'input.plotMajorGrid == true',
                                      checkboxInput("plotMinorGrid",
@@ -131,7 +143,7 @@ ui <- fluidPage(
                                                    FALSE)),
                                    hr())),
                    tabPanel("Theme and colors",
-                            
+
                             column(12,
                                    selectInput("plotTheme",
                                                label = h5("Theme"),
@@ -164,7 +176,7 @@ ui <- fluidPage(
                                      )
                                    )),
                             column(12,
-                                   
+
                                    hr())),
                    tabPanel("Titles",
                             column(12,
@@ -205,7 +217,7 @@ ui <- fluidPage(
                                                  "Middle" = 0.5,
                                                  "Right" = 1),
                                                selected = 0),
-                                   
+
                                    selectInput("xAxisvjust",
                                                label = h5("Vertical Scale Justification"),
                                                choices = list(
@@ -213,7 +225,7 @@ ui <- fluidPage(
                                                  "Middle" = 0.5,
                                                  "Bottom" = 1),
                                                selected = 0),
-                                   
+
                                    checkboxInput("autoScale",
                                                  "Automatic Scale Limits",
                                                  TRUE)),
@@ -250,9 +262,9 @@ ui <- fluidPage(
                   tabPanel("Plot",
                            br(),
                            withSpinner(
-                             plotOutput("rmcorrPlot", 
+                             plotOutput("rmcorrPlot",
                                         height = "auto"))
-                           
+
                   ),
                   tabPanel("R Code",
                            column(12,
@@ -267,7 +279,7 @@ ui <- fluidPage(
                            htmlOutput("rmcorrDownload")),
                   tabPanel("About",
                            includeHTML("www/about.html"))
-                  
+
       )
     )
   )
@@ -275,32 +287,32 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
-  
+
   observe({
     session$setCurrentTheme(
       if (isTRUE(input$dark_mode)) dark else light
     )
   })
-  
+
   # Read the input data.
   inputData <- callModule(dataUpload,"rmcorr")
-  
-  
+
+
   # Process the data. This is a reactive depending on the inputData!
   processedData <- reactive({
     req(input$CIlevel)
     dataManipulation(input, inputData)
-  }) 
-  
+  })
+
   dataManipulation <- function(input, inputData) {
-    
+
     subColumn <- input$subColumn
     m1Column <- input$m1Column
     m2Column <- input$m2Column
-    
+
     cleanedData <- select(inputData$inputData(),all_of(c(subColumn, m1Column, m2Column)))
     df <- reactive({cleanedData}) %>% bindCache(cleanedData)
-    
+
     my.rmc = reactive({
       req(input$subColumn)
       rmcorr(participant = subColumn,
@@ -312,11 +324,11 @@ server <- function(input, output, session) {
              nreps = input$bootstrapnreps,
              bstrap.out = input$bootstrapout)
     })
-    
+
     n = reactive({
       length(unique(cleanedData[[subColumn]]))
     }) %>% bindCache(unique(cleanedData[[subColumn]]))
-    
+
     ##Add code for calculating rmcorr
     code <- reactive({
       glue('## Get sample size
@@ -331,7 +343,7 @@ my.rmc <- rmcorr(participant = {subColumn},
                  nreps = {input$bootstrapnreps},
                  bstrap.out = {input$bootstrapout})\n\n')
     })
-    
+
     return(list(
       rmc = my.rmc,
       df = df,
@@ -339,37 +351,46 @@ my.rmc <- rmcorr(participant = {subColumn},
       code = code
     ))
   }
-  
+
   output$bootstrapResamples <- renderTable({
     # We don't render the table without inputData.
     req(inputData$name())
     processedData()$rmc()$resamples
   }) %>% bindCache(processedData()$rmc()$resamples)
-  
+
+  # UI - legend title
+  output$legendTitleUI <- renderUI({
+    req(inputData$name())
+    tagList(
+      textInput("legendTitle",
+            label = "Legend Title",
+            value = input$subColumn)
+    )})
+
   # UI - Data - Filter the data.
   output$DataFilterColumnsUI <- renderUI({
     req(inputData$conditions()) #conditions are the column names
     tagList(
       selectInput('subColumn',
                   label = HTML("<h5>Detected columns</h5>
-                             Subject column:"), 
+                             Subject column:"),
                   choices = inputData$conditions(),
                   selected = inputData$conditions()[1],
                   multiple = FALSE),
       selectInput('m1Column',
-                  label = HTML("Measure 1 column"), 
+                  label = HTML("Measure 1 column"),
                   choices = inputData$conditions(),
                   selected = inputData$conditions()[2],
                   multiple = FALSE),
       selectInput('m2Column',
-                  label = HTML("Measure 2 column:"), 
+                  label = HTML("Measure 2 column:"),
                   choices = inputData$conditions(),
                   selected = inputData$conditions()[3],
                   multiple = FALSE)
     )
   })
   outputOptions(output, "DataFilterColumnsUI", suspendWhenHidden = FALSE)
-  
+
   # update plot titles:
   observeEvent(input$m1Column, {
     updateTextInput(session, "xAxisTitle", value = input$m1Column)}
@@ -377,13 +398,13 @@ my.rmc <- rmcorr(participant = {subColumn},
   observeEvent(input$m2Column, {
     updateTextInput(session, "yAxisTitle", value = input$m2Column)}
   )
-  
-  
-  #Need warnings for missing data, non-numeric input in X and Y? 
-  
+
+
+  #Need warnings for missing data, non-numeric input in X and Y?
+
   # Generate the plot code based on input options but do not evaluate yet.
   plotCode <- reactive({createPlot(input)}) %>% bindCache({createPlot(input)})
-  
+
   # Evaluate the code based on the processed data.
   plotFigure <- reactive({
     plotData <- inputData$inputData()
@@ -391,7 +412,7 @@ my.rmc <- rmcorr(participant = {subColumn},
     n <- processedData()$n()
     eval(parse(text = glue(plotCode())))
   })
-  
+
   # Render the plot.
   output$rmcorrPlot <- renderPlot({
     # We don't render the plot without inputData.
@@ -399,10 +420,10 @@ my.rmc <- rmcorr(participant = {subColumn},
     plotFigure()},
     height = function(x) input$height,
     width = function(x) input$width
-    
-  ) 
-  
-  
+
+  )
+
+
   # ScriptCode
   scriptCode <- reactive({
     # cat(file=stderr(), processedData()$code())
@@ -410,16 +431,16 @@ my.rmc <- rmcorr(participant = {subColumn},
                , plotCode()
     )
   })
-  
-  
+
+
   output$rmcorrCode <- renderText({
     # We don't render the code without inputData.
     req(inputData$name())
     # inputData$code()
     scriptCode()
-    
+
   }) %>% bindCache(scriptCode())
-  
+
   # Print the data
   output$rmcorrResults <- renderUI({
     req(inputData$name())
@@ -427,22 +448,22 @@ my.rmc <- rmcorr(participant = {subColumn},
     str_rrm <- paste("Repeated measures correlation: ", round(processedData()$rmc()$r, digits = 3))
     str_df  <- paste("Degrees of freedom: ", processedData()$rmc()$df)
     str_p   <- paste("p-value:", round(processedData()$rmc()$p, digits = 3))
-    str_CI  <- paste(CIlevel,"% Confidence Interval: ", 
+    str_CI  <- paste(CIlevel,"% Confidence Interval: ",
                      paste0(round(processedData()$rmc()$CI[1], digits = 3), sep = ", ", round(processedData()$rmc()$CI[2], digits = 3)), sep = "")
     HTML(paste(str_rrm, str_df, str_p, str_CI, sep = '</br>'))
-    
+
   })
-  
+
   output$rmcorrReportable <- renderUI({
     req(inputData$name())
     CIlevel <- processedData()$rmc()$CI.level * 100
-    HTML(glue("r<sub>rm</sub>({processedData()$rmc()$df}) = 
-              {round(processedData()$rmc()$r, digits = 2)}, 
+    HTML(glue("r<sub>rm</sub>({processedData()$rmc()$df}) =
+              {round(processedData()$rmc()$r, digits = 2)},
               {CIlevel}% CI [{round(processedData()$rmc()$CI[1], digits = 3)},
-              {round(processedData()$rmc()$CI[2], digits = 3)}], 
+              {round(processedData()$rmc()$CI[2], digits = 3)}],
               {ifelse(processedData()$rmc()$p < .001, 'p < 0.001', paste('p = ',round(processedData()$rmc()$p, digits = 3),sep = ''))}"))
   })
-  
+
   output$rmcorrDataSummary <- renderPrint({
     # We don't render the table without inputData.
     req(inputData$name())
@@ -455,18 +476,18 @@ my.rmc <- rmcorr(participant = {subColumn},
               k<sub>max</sub> = {max(table(inputData$inputData()[[input$subColumn]]))}
               <h5>Dataset:</h5>"))
   })
-  
+
   output$rmcorrData <- renderTable({
     # We don't render the table without inputData.
     req(inputData$name())
     inputData$inputData()
   })
-  
+
   output$rmcorrDownload <- renderUI({
     req(inputData$name())
     tagList(
       h4("Download the plot"),
-      p("Select the image format or download a zip file with all the 
+      p("Select the image format or download a zip file with all the
                images, the script and data used to generate the plot."),
       selectInput("downloadFormat",
                   label = "Image format",
@@ -481,18 +502,18 @@ my.rmc <- rmcorr(participant = {subColumn},
                       "png" = "png")
                   ),
                   selected = "pdf"),
-      downloadButton("downloadPlot", 
+      downloadButton("downloadPlot",
                      label = "Download Image"),
       br(),
       downloadButton('downloadZip',
                      label = 'Download Zip')
     )
   })
-  
+
   # Download button
   output$downloadPlot <- downloadHandler(
     filename = function() {
-      paste(paste('rmcorrPlot-',inputData$name(), sep = ""), 
+      paste(paste('rmcorrPlot-',inputData$name(), sep = ""),
             input$downloadFormat, sep = ".")
     },
     content = function(file) {
@@ -501,7 +522,7 @@ my.rmc <- rmcorr(participant = {subColumn},
                plot = plotFigure(),
                device = input$downloadFormat,
                # Width and height are in inches. We increase the dpi to 300, so we
-               # have to divide by 72 (original default pixels per inch) 
+               # have to divide by 72 (original default pixels per inch)
                width = input$width / 72,
                height = input$height / 72,
                compression = "lzw",
@@ -518,7 +539,7 @@ my.rmc <- rmcorr(participant = {subColumn},
       }
     }
   )
-  
+
   # Download zip file with script, data, and plots.
   output$downloadZip <- downloadHandler(
     filename = function() {
@@ -530,20 +551,20 @@ my.rmc <- rmcorr(participant = {subColumn},
       # Copy inputData to tmpDir
       file.copy(from = c(inputData$datapath()),
                 to = tmpdir)
-      
+
       # Move to the tmpDir to work with the tmpFiles
       setwd(tmpdir)
-      
+
       # Change the name of the uploaded file so that the code still works.
       tmpInputFile <- basename(inputData$datapath())
       file.rename(from = tmpInputFile,
                   to = inputData$name())
-      
+
       # Code
       write(scriptCode(), "rmcorrPlot.R")
-      
+
       fileList <- c(fileList, inputData$name(), "rmcorrPlot.R")
-      
+
       # Create all images (except tiff that is compressed).
       for (format in c('pdf','svg','eps','png')) {
         file <- paste(paste0('rmcorrPlot-',inputData$name()),
@@ -557,7 +578,7 @@ my.rmc <- rmcorr(participant = {subColumn},
                dpi = 300)
         fileList <- c(fileList, file)
       }
-      
+
       # Add compressed .tiff
       tiffFile <- paste(paste0('rmcorrPlot-',inputData$name()),
                         'tiff', sep = ".")
@@ -570,12 +591,12 @@ my.rmc <- rmcorr(participant = {subColumn},
              units = "in",
              dpi = 300)
       fileList <- c(fileList, tiffFile)
-      
+
       # And create the zip
       zip(zipfile=fname, files=fileList)
     },
     contentType = "application/zip"
   )
-  
+
 }
 shinyApp(ui = ui, server = server)
